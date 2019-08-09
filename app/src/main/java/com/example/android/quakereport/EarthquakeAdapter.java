96d03e47e377a6387b61e9ee71877ed0ab561eeb
TianSirk <tiansirk@outlook.hu>
2019-08-09 21:15:38 +0200

    Separate date to time and date and separate location to location and vicinity

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -9,8 +9,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 /**
  * Created by tians on 2019. 07. 27..
@@ -41,20 +42,44 @@ public class EarthquakeAdapter extends ArrayAdapter {
         // set this text on the name TextView
         magnitudeTextView.setText(Double.toString(currEarthquake.getMagnitude()));
 
+        // Find the TextView in the quake_item.xml layout with the ID location_vicinity
+        TextView locationVicinityTextView = (TextView) listItemView.findViewById(R.id.location_vicinity);
+        // Get the location name from the current EarthQukae object and
+        // set this text on the name TextView
+        locationVicinityTextView.setText(currEarthquake.getLocation());
+
         // Find the TextView in the quake_item.xml layout with the ID location
         TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
         // Get the location name from the current EarthQukae object and
         // set this text on the name TextView
         locationTextView.setText(currEarthquake.getLocation());
 
+        // Find the TextView in the quake_item.xml layout with the ID time
+        TextView timeTextView = (TextView) listItemView.findViewById(R.id.time);
+        // Get the date from the current EarthQukae object and
+        // set this text on the name TextView
+        timeTextView.setText(formatTime(currEarthquake.getTimeInMillisec()));
+
         // Find the TextView in the quake_item.xml layout with the ID date
         TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
         // Get the date from the current EarthQukae object and
         // set this text on the name TextView
-        dateTextView.setText(currEarthquake.getDate());
+        dateTextView.setText(formatDate(currEarthquake.getTimeInMillisec()));
 
         // Return the whole list item layout (containing 3 TextViews)
         // so that it can be shown in the ListView
         return listItemView;
+    }
+
+    private static String formatTime (long unixTime){
+        Date time = new Date(unixTime);
+        SimpleDateFormat formattedDate = new SimpleDateFormat("kk:mm:ss");
+        return formattedDate.format(time);
+    }
+
+    private static String formatDate (long unixTime){
+        Date date = new Date(unixTime);
+        SimpleDateFormat formattedDate = new SimpleDateFormat("yyyy:MM:dd");
+        return formattedDate.format(date);
     }
 }
