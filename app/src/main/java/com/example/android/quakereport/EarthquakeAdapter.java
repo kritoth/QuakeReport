2da0b353472d86aed7095dbf6f8ee7c829c365f9
BF8EZM <kritoth@gmail.com>
2019-08-12 14:13:42 +0200

    Correct color switch for magnitudes and test cases

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -85,7 +85,40 @@ public class EarthquakeAdapter extends ArrayAdapter {
     private int getMagnitudeColor(double magnitude) {
         int color = 0;
         switch ((int)magnitude){
+            case 0:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude1);
+                break;
+            case 1:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude1);
+                break;
+            case 2:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude2);
+                break;
+            case 3:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude3);
+                break;
+            case 4:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude4);
+                break;
+            case 5:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude5);
+                break;
+            case 6:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude6);
+                break;
+            case 7:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude7);
+                break;
             case 8:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude8);
+                break;
+            case 9:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude9);
+                break;
+            case 10:
+                color = ContextCompat.getColor(getContext(), R.color.magnitude10plus);
+                break;
+            default:
                 color = ContextCompat.getColor(getContext(), R.color.magnitude10plus);
                 break;
         }
